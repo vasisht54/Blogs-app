9c924fad2d3278bf3f07037479e1fc089f557aaf
vasisht54 <vasisht.theviper@gmail.com>
2021-03-21 15:31:10 -0700

    Added json server

--- src/Home.js
+++ src/Home.js
@@ -1,38 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+
   const handleDelete = (id) => {
     setBlogs(blogs.filter((blog) => blog.id !== id));
   };
 
-  const [blogs, setBlogs] = useState([
-    {
-      title: "React Hooks and Context",
-      body:
-        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum molestiae quod eum voluptate aspernatur! Aut incidunt suscipit iure numquam esse rerum eius et dolorum, cupiditate error, eum temporibus nemo! Saepe.",
-      author: "M. Scott",
-      id: 1,
-    },
-    {
-      title: "Redux for state Management",
-      body:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, aspernatur illo, natus perspiciatis modi pariatur sit nobis magni at nemo quis voluptas consequatur. Reprehenderit explicabo laudantium aspernatur, quidem ipsa libero.",
-      author: "J. Halpert",
-      id: 2,
-    },
-    {
-      title: "Web Dev with React!",
-      body:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id illum non dolorum eaque quasi. Eligendi obcaecati incidunt pariatur odio ea. Animi tempore repudiandae quis totam nisi. In porro totam provident!",
-      author: "Oscar Martinez",
-      id: 3,
-    },
-  ]);
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} />}
     </div>
   );
 };
