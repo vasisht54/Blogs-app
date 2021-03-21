5e5d8b3d10d986bddd45e9f4d3e9602449591e12
vasisht54 <vasisht.theviper@gmail.com>
2021-03-20 22:50:01 -0700

    First commit

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>Blogs</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">New blog</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
